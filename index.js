ea073eeecd1c495ec1164d691e6d652f07ab12e1
Oran RH <oranroha@gmail.com>
2021-07-31 13:55:44 -0400

    removed unused

--- index.js
+++ index.js
@@ -130,13 +130,3 @@ function getFiles(path) {
     });
     return (fileList);
 }
-
-function sum(obj) {
-    var sum = 0;
-    for (var el in obj) {
-        if (obj.hasOwnProperty(el)) {
-            sum += parseFloat(obj[el]);
-        }
-    }
-    return sum;
-}
